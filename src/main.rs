217b3c8ef787aea3892b613098ec8da30cfd5926
alexxmatei <alexxmatei@gmail.com>
2023-03-10 13:08:43 +0200

    Changed the implementation of binary search algo Instead of actually splitting the array, we now use a low_index and high_index to search through the array.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,49 @@
 fn main() {
-    let mut my_array: &mut [i32] = &mut [3, 1, 8, 15, 25, 26];
-    let searched_element = 8;
-    let mut element_found = false;
+    let mut my_array = [3, 1, 8, 15, 6, 32];
+    let mut low_index: u8 = 0;
+    let mut high_index: u8 = (my_array.len() - 1) as u8;
+    let mut middle_index: u8;
+    const TARGET_NUMBER: i32 = 7;
+    println!("Target number: {}", TARGET_NUMBER);
+    println!("Initial array: {:?}", my_array);
     my_array.sort();
-    print!("{}", my_array[my_array.len() / 2]);
-    while (my_array.len() > 1 && !element_found) {
-        let element = my_array[my_array.len() / 2];
-        if element == searched_element {
-            print!("We found the number at index {} ", my_array.len() / 2);
-            element_found = true;
-        } else if element < searched_element {
-            print!("Element is smaller than searched element, look in 2nd half.\n");
-            let (_, right_half) = my_array.split_at_mut(my_array.len() / 2);
-            my_array = right_half;
-            print!("{:?}\n", my_array);
-        } else if element > searched_element {
-            print!("Element is bigger than searched element, look in 1st half.\n");
-            let (left_half, _) = my_array.split_at_mut(my_array.len() / 2);
-            my_array = left_half;
-            print!("{:?}\n", my_array);
+    println!("Sorted array: {:?}", my_array);
+
+    println!("Starting binary search algorihm...\n");
+
+    loop {
+        print!("Searching the middle of the array...\n");
+        middle_index = (low_index + high_index) / 2;
+        let middle_number = my_array[middle_index as usize];
+        println!("Found the number: {}", middle_number);
+        if middle_number == TARGET_NUMBER {
+            println!("Target number found at index {}", middle_index);
+            break;
         }
-    }
-    if my_array[0] == searched_element {
-        element_found = true;
-        print!("We found the number at index {} ", 0);
-    }
-    if !element_found {
-        print!("We didn't find the element.")
+        if low_index == high_index {
+            println!("Couldn't find target number in the given array.");
+            break;
+        }
+
+        if TARGET_NUMBER < middle_number {
+            println!(
+                "Found number is higher than target number: {}",
+                TARGET_NUMBER
+            );
+            println!("Creating a new array from the 1st half of the old array...");
+            high_index = middle_index - 1;
+        } else if TARGET_NUMBER > middle_number {
+            print!(
+                "Found number is lower than than target number: {}\n",
+                TARGET_NUMBER
+            );
+            println!("Creating a new array from the 2nd half of the old array...");
+            low_index = middle_index + 1;
+        }
+
+        println!(
+            "New array: {:?}\n",
+            &my_array[low_index as usize..(high_index + 1) as usize]
+        )
     }
 }
